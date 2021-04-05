9d5a8b91c7ded235b3a32a2e2d43ea47740675e5
nima-abdpoor <nimaabdi328@yahoo.com>
2021-04-05 11:27:38 +0430

    insert to tree

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,13 @@
 
 
 fun main(){
-
+    val node = Node(50)
+    val tree = BinarySearchTree(node)
+    tree.insert(30)
+    tree.insert(20)
+    tree.insert(40)
+    tree.insert(70)
+    tree.insert(60)
+    tree.insert(80)
+    tree.inorder()
 }
