908a132ad633241371f71e56f2fbf4ae2e021920
nima-abdpoor <nimaabdi328@yahoo.com>
2021-04-05 11:23:49 +0430

    BinarySearchTree.kt class completed.

--- src/BinarySearchTree.kt
+++ src/BinarySearchTree.kt
@@ -1,3 +1,26 @@
-class BinarySearchTree {
+class BinarySearchTree(private var root : Node) {
+    private fun insertRec(_node: Node?, key: Int): Node {
+        var node  = _node
+        if (node == null) {
+            node = Node(key)
+            return node
+        }
+        if (key < node.key) node.left = insertRec(node.left, key)
+        else if (key > node.key) node.right = insertRec(node.right, key)
+        return node
+    }
+    fun insert(key : Int){
+        root = insertRec(root,key)
+    }
+    fun inorder(){
+        inorderRec(root)
+    }
 
+    private fun inorderRec(_node: Node?) {
+        _node?.let {
+            inorderRec(_node.left)
+            println(_node.key)
+            inorderRec(_node.right)
+        }
+    }
 }
